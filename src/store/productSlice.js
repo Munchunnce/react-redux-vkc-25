4d389ccbb25ddd84aeb0b38015ae7ac51617c677
Vimal <vimalkumarak478@gmail.com>
2025-09-22 23:53:51 +0530

    cart productSlice create vkc

---
+++ src/store/productSlice.js
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const STATUSES = Object.freeze({
+    IDLE: 'idle',
+    ERROR: 'error',
+    LOADING: 'loading'
+});
+const productSlice = createSlice({
+    name: 'product',
+    initialState: {
+        data: [],
+        status: STATUSES.IDLE
+    },
+    reducers: {
+        setProducts(state, action) {
+            state.data = action.payload;
+        },
+        setStatus(state, action) {
+            state.status = action.payload;
+        },
+    },
+});
+
+export const { setProducts, setStatus } = productSlice.actions;
+export default productSlice.reducer;
+
+//Thunk
+export function fetchProduct() {
+    async function fetchProductThunk(dispatch, getState) {
+        dispatch(setStatus(STATUSES.LOADING));
+        try {
+            const res = await fetch('https://fakestoreapi.com/products');
+            const data = await res.json();
+            dispatch(setProducts(data));
+        } catch (err) {
+            console.log(err);
+            dispatch(setStatus(STATUSES.ERROR));;
+        }
+        
+    }
+
+    fetchProductThunk();
+}
